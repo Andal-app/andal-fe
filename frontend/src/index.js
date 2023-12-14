cce5daf43818094b90fd952be930efc6fb840255
Brandon Cynwell <cynwellbrandon@gmail.com>
2023-12-14 16:54:21 +0700

    Improve performance and SEO

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-// import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
-// import axios from 'axios';
 
 // styling
 import 'bulma/css/bulma.css';
 import './styles/style2.css';
 import './styles/style.css';
-import 'bulma-extensions/bulma-timeline/dist/css/bulma-timeline.min.css';
 
 // axios.defaults.withCredentials = true;
 
