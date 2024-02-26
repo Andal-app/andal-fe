79cb5a226cd58553d646b539c78b8c19758c53e1
raisasalsabily <raisa.salsabil@mail.ugm.ac.id>
2024-02-26 19:51:54 +0700

    fix(profile): finalize profile page (mobile)

--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -6,16 +6,25 @@ import ProfPic from '../../assets/images/profile_picture.jpeg';
 
 import { BiEdit } from 'react-icons/bi';
 import { FaChevronRight } from 'react-icons/fa6';
+import ProfSetItem from './ProfSetItem';
 
 function Profile() {
+  const ParentSettingItems = [
+    { link: '', icon: 'bx:edit', text: 'Edit profil' },
+    { link: '', icon: 'mingcute:question-line', text: 'Bantuan penggunaan' },
+    { link: '', icon: 'material-symbols:lock-outline', text: ' Ganti pasword' },
+    { link: '', icon: 'tabler:logout', text: 'Keluar' },
+    { link: '', icon: 'typcn:delete-outline', text: 'Hapus akun', redText: true }
+  ];
+
   return (
     <div>
       <Sidebar />
 
       {/* user profile start */}
-      <div id="user__profile" className={`bg-red-300 flex flex-col items-center gap-6 py-12`}>
+      <div id="user__profile" className={`flex flex-col items-center gap-6 py-12`}>
         <img id="user__profile__picture" src={ProfPic} className={`w-36 h-36 rounded-full`}></img>
-        <div className="bg-neutral-200 text-center">
+        <div className="text-center">
           <p className={`text-b-xl font-bold`}>Raisa Salsabil Yusriyya</p>
           <p className={`text-b-lg`}>Orang tua</p>
         </div>
@@ -25,13 +34,9 @@ function Profile() {
       {/* user setting list start */}
       <div id="user__setting__list">
         <ul>
-          <li className="h-16 flex items-center justify-between px-12 border-b border-neutral-300 hover:bg-violet-50 duration-300">
-            <Link to="" className="flex items-center gap-6">
-              <Icon icon={'bx:edit'} className="w-6 h-6 text-violet-600" />
-              <span className="text-b-xl">Edit profil</span>
-            </Link>
-            <Icon icon={'uiw:right'} className="w-6 h-6 text-neutral-600" />
-          </li>
+          {ParentSettingItems.map(({ link, icon, text, redText, index }) => (
+            <ProfSetItem link={link} icon={icon} text={text} redText={redText} index={index} />
+          ))}
         </ul>
       </div>
       {/* user setting list end*/}
