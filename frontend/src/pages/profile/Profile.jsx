6fe895536c11069d92cf5e7a2addf5af68bb59a2
raisasalsabily <raisa.salsabil@mail.ugm.ac.id>
2024-02-26 20:05:44 +0700

    fix(profile): finalize profile page for desktop version

--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -18,28 +18,34 @@ function Profile() {
   ];
 
   return (
-    <div>
+    <div className="flex">
       <Sidebar />
 
-      {/* user profile start */}
-      <div id="user__profile" className={`flex flex-col items-center gap-6 py-12`}>
-        <img id="user__profile__picture" src={ProfPic} className={`w-36 h-36 rounded-full`}></img>
-        <div className="text-center">
-          <p className={`text-b-xl font-bold`}>Raisa Salsabil Yusriyya</p>
-          <p className={`text-b-lg`}>Orang tua</p>
-        </div>
-      </div>
-      {/* user profile end */}
+      <main className="w-full lg:w-1/2 lg:border-r lg:border-neutral-300">
+        {/* page title start */}
+        <h1 className="hidden lg:block text-b-xl font-bold bg-violet-200 text-center py-6">Profil Pengguna</h1>
+        {/* page title end */}
 
-      {/* user setting list start */}
-      <div id="user__setting__list">
-        <ul>
-          {ParentSettingItems.map(({ link, icon, text, redText, index }) => (
-            <ProfSetItem link={link} icon={icon} text={text} redText={redText} index={index} />
-          ))}
-        </ul>
-      </div>
-      {/* user setting list end*/}
+        {/* user profile start */}
+        <div id="user__profile" className={`flex flex-col items-center gap-6 py-12`}>
+          <img id="user__profile__picture" src={ProfPic} className={`w-36 h-36 rounded-full`}></img>
+          <div className="text-center">
+            <p className={`text-b-xl font-bold`}>Raisa Salsabil Yusriyya</p>
+            <p className={`text-b-lg`}>Orang tua</p>
+          </div>
+        </div>
+        {/* user profile end */}
+
+        {/* user setting list start */}
+        <div id="user__setting__list">
+          <ul>
+            {ParentSettingItems.map(({ link, icon, text, redText, index }) => (
+              <ProfSetItem link={link} icon={icon} text={text} redText={redText} index={index} />
+            ))}
+          </ul>
+        </div>
+        {/* user setting list end*/}
+      </main>
     </div>
   );
 }
