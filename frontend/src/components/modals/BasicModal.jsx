9f673ffbf46c67b9aee5d4cdd3cb6faa2d24879c
raisasalsabily <raisa.salsabil@mail.ugm.ac.id>
2024-03-06 11:12:56 +0700

    feat(modal): create BasicModal close button

--- frontend/src/components/modals/BasicModal.jsx
+++ frontend/src/components/modals/BasicModal.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import TextInput from '../inputs/TextInput';
 import SubmitBtn from '../buttons/SubmitBtn';
+import { Icon } from '@iconify/react';
 
 const BasicModal = ({ toggleConnectModal, handlePropagation }) => {
   return (
@@ -13,11 +14,21 @@ const BasicModal = ({ toggleConnectModal, handlePropagation }) => {
       {/* modal box start */}
       <div id="modal__box" onClick={handlePropagation} className="bg-white w-[360px] rounded-xl p-4">
         <form className="flex flex-col gap-4">
+          {/* close button start */}
+          <div className="flex justify-end">
+            <button id="close__button" onClick={toggleConnectModal}>
+              <Icon icon="mingcute:close-line" className={`w-6 h-6 text-neutral-600 `} />
+            </button>
+          </div>
+          {/* close button end */}
+
+          {/* modal title start */}
           <p className="font-bold text-violet-900 text-b-xl text-center">Masukkan Username Anak</p>
+          {/* modal title end */}
 
           <TextInput type="text" name="username" id="username" placeholder="" required />
 
-          <p id="error__message" className="text-red-600 text-b-sm">
+          <p id="error__message" className="hidden text-red-600 text-b-sm">
             Username tidak ditemukan
           </p>
 
