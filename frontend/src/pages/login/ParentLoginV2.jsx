f56d8918e75361e3bd8bf3feb88e76bc9b7ee5fe
raisasalsabily <raisa.salsabil@mail.ugm.ac.id>
2024-03-08 09:44:14 +0700

    fix(layout): membenarkan RegisterLayout
    RegisterLayout sebelumnya belum menggunakan BasicLayot sehingga ditambahkan.

--- frontend/src/pages/login/ParentLoginV2.jsx
+++ frontend/src/pages/login/ParentLoginV2.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import RegisterLayout from '../../layouts/auth/RegisterLayout';
 import LoginLayout from '../../layouts/LoginLayout';
 
 function ParentLoginV2() {
-  return <LoginLayout>test</LoginLayout>;
+  return <RegisterLayout>test reg layout</RegisterLayout>;
 }
 
 export default ParentLoginV2;
