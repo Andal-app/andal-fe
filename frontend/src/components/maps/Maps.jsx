3863b14dc8e709c07112ebe6f186593c8dbf54f2
raisasalsabily <raisa.salsabil@mail.ugm.ac.id>
2024-05-14 14:58:24 +0700

    feat-enable pindah marker ketika klik pada map

--- frontend/src/components/maps/Maps.jsx
+++ frontend/src/components/maps/Maps.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect } from 'react';
-import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
+import React, { useEffect, useState } from 'react';
+import { MapContainer, Marker, Popup, TileLayer, useMap, ZoomControl, useMapEvents } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import MapPinImg from '../../assets/images/map_pin.png';
 
 const icon = L.icon({
   iconUrl: MapPinImg,
-  iconSize: [38, 38]
+  iconSize: [50, 50]
 });
 
 const centerPosition = [-7.7761951, 110.3762101];
@@ -28,6 +28,24 @@ function ResetCenterView({ selectPosition }) {
 export default function Maps({ selectPosition, setSelectPosition }) {
   const locationSelection = selectPosition ? [selectPosition.lat, selectPosition.lon] : centerPosition;
 
+  const [markerPosition, setMarkerPosition] = useState(centerPosition);
+
+  const MapEvents = () => {
+    const map = useMap();
+
+    useMapEvents({
+      click(e) {
+        const { lat, lng } = e.latlng;
+        setMarkerPosition([lat, lng]);
+        setSelectPosition({ lat, lon: lng });
+        map.flyTo([lat, lng], map.getZoom(), {
+          duration: 500
+        });
+      }
+    });
+    return null;
+  };
+
   const handleMarkerDragEnd = (event) => {
     const marker = event.target;
     const newPosition = marker.getLatLng();
@@ -38,14 +56,22 @@ export default function Maps({ selectPosition, setSelectPosition }) {
   };
 
   return (
-    <MapContainer center={centerPosition} zoom={13} scrollWheelZoom={true} style={{ width: '100%', height: '100%' }}>
+    <MapContainer
+      center={centerPosition}
+      zoom={13}
+      scrollWheelZoom={true}
+      zoomControl={false}
+      style={{ width: '100%', height: '100%' }}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         // url="https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=fpDlF5YXcAYQ19hRzNed"
       />
 
-      {selectPosition && (
+      <ZoomControl position="bottomright" />
+
+      {/* {selectPosition && (
         <Marker
           position={locationSelection}
           icon={icon}
@@ -58,7 +84,22 @@ export default function Maps({ selectPosition, setSelectPosition }) {
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
         </Marker>
-      )}
+      )} */}
+
+      <Marker
+        position={selectPosition ? locationSelection : markerPosition}
+        icon={icon}
+        draggable={true}
+        eventHandlers={{
+          dragend: handleMarkerDragEnd
+        }}
+      >
+        <Popup>
+          Marker added here! <br /> Lat: {markerPosition[0].toFixed(6)}, Lon: {markerPosition[1].toFixed(6)}
+        </Popup>
+      </Marker>
+
+      <MapEvents />
 
       <ResetCenterView selectPosition={selectPosition} />
     </MapContainer>
